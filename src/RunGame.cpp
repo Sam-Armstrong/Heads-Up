c45ec85d6a29ca712b24daa9b2ce8096a37ee740
Sam Armstrong <samuel_e_armstrong@yahoo.co.uk>
2023-01-22 00:01:28 +0000

    Added RunGame

---
+++ src/RunGame.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#ifndef GAME_H
+#define GAME_H
+#include "Game.h"
+#endif
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
